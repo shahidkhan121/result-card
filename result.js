1629da9bab3fcc1f88a70242a1cb7e6d89c4d72d
shahidkhan121 <shah12pp@gmail.com>
2024-01-09 12:38:49 +0500

    Create result.js
    .

---
+++ result.js
@@ -0,0 +1,34 @@
+let marks = 40;
+if (marks > 90) {
+    console.log("BRILLENT")
+}
+else if (marks > 85) {
+    console.log("YOUR CHILD GOT A GRADE")
+}
+else if (marks>80) {
+    console.log("your child got A- grade")
+}
+else if ( marks > 75) {
+    console.log("your chil got B+ grade")
+}
+else if (marks > 70) { 
+    console.log("your child got B grade")
+}
+else if (marks > 65) {
+    console.log("YOUR CHLD GOT B- GRADE")
+}
+else if (marks > 61) {
+    console.log("your child got C+ grade")
+}
+else if ( marks > 58) {
+    console.log("your child got c grade")
+}
+else if ( marks > 55) { 
+    console.log("your child got c- grade")
+}
+else if ( marks >= 50) {
+    console.log("your chld got d grade")
+}
+else {
+    console.log(" try again next year you are failed")
+}
